581b2df83bc0eec68273f1378552579b901ae66e
Zachary Cauchi <zach.cauchi@hotmail.com>
2025-03-14 22:38:05 +0100

    feat: Use defmt and espflash for log and flashing

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() {
     linker_be_nice();
+    println!("cargo:rustc-link-arg=-Tdefmt.x");
     // make sure linkall.x is the last linker script (otherwise might cause problems with flip-link)
     println!("cargo:rustc-link-arg=-Tlinkall.x");
 }
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,12 @@
 #![no_std]
 #![no_main]
 
+use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_hal::clock::CpuClock;
 use esp_hal::timer::timg::TimerGroup;
+use esp_println as _;
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
@@ -25,6 +27,8 @@ async fn main(spawner: Spawner) {
     let timer0 = TimerGroup::new(peripherals.TIMG1);
     esp_hal_embassy::init(timer0.timer0);
 
+    info!("Embassy initialized!");
+
     let timer1 = TimerGroup::new(peripherals.TIMG0);
     let _init = esp_wifi::init(
         timer1.timer0,
@@ -37,6 +41,7 @@ async fn main(spawner: Spawner) {
     let _ = spawner;
 
     loop {
+        info!("Hello world!");
         Timer::after(Duration::from_secs(1)).await;
     }
 
